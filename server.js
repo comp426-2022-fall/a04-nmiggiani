8e0b64265cc4db82ab46488f3a0b996c8a857190
nmiggiani <36214382+nmiggiani@users.noreply.github.com>
2022-11-28 16:13:10 -0500

    try it2

---
+++ server.js
@@ -0,0 +1,53 @@
+import minimist from "minimist"
+import express from "express"
+import { roll } from './lib/roll.js'
+
+
+const app = express()
+
+const args = minimist(process.argv.slice(2))
+
+app.use(express.urlencoded({ extended: true }))    
+
+app.get('/app/', (req, res) => {                   
+    res.send('200 OK');
+})
+
+
+app.post('/app/roll', (req, res) => {                
+    const sides = parseInt(req.body.sides)         
+    const dice = parseInt(req.body.dice)
+    const rolls = parseInt(req.body.rolls)
+
+    res.send(roll(sides, dice, rolls))
+})
+
+app.get('/app/roll/', (req, res) => {               
+	res.send(roll(6,2,1))                           
+});
+
+
+app.get('/app/roll/:sides/', (req, res) => {        
+    const sides = parseInt(req.params.sides)
+    res.send(roll(sides, 2, 1))
+})
+
+
+app.get('/app/roll/:sides/:dice/:rolls', (req, res) => {        
+    const sides = parseInt(req.params.sides)
+    const dice = parseInt(req.params.dice)
+    const rolls = parseInt(req.params.rolls)
+    res.send(roll(sides, dice, rolls))
+})
+
+app.get('/app/roll/:sides/:dice', (req, res) => {   
+    const sides = parseInt(req.params.sides)
+    const dice = parseInt(req.params.dice)
+    res.send(roll(sides, dice, 1))
+})
+
+app.listen(args.port || 5000)       
+
+app.use((req, res) => {                       
+    res.status(404).send("404")
+})
