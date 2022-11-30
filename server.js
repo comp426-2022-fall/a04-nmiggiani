d09952e964f5b5c3ab4a7fb99ec64cc9afbed460
nmiggiani <36214382+nmiggiani@users.noreply.github.com>
2022-11-30 13:58:49 -0500

    complete

--- server.js
+++ server.js
@@ -48,7 +48,7 @@ app.get('/app/roll/:sides/:dice', (req, res) => {
 
 
 app.use((req, res) => {                       
-    res.status(404).send("404")
+    res.status(404).send("404 NOT FOUND")
 })
 
 app.listen(args.port || 5000)       
